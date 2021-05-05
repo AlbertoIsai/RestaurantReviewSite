98b6a4003d9ef7579a3c0f734c6cfcdc3f4ba585
Alberto Lopez <Albertoisai15@gmail.com>
2021-05-05 11:31:21 -0500

    Updated restaurant controller

--- src/main/java/com/cognixia/jump/controller/RestaurantController.java
+++ src/main/java/com/cognixia/jump/controller/RestaurantController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.model.Restaurant;
+import com.cognixia.jump.model.Review;
 import com.cognixia.jump.repository.RestaurantRepository;
 
 @RestController
@@ -22,10 +26,21 @@ public class RestaurantController {
 		return restaurants;
 	}
 	
-	@GetMapping(value = "/restaurant/{customerId}")
+	@GetMapping(value = "/restaurant/{restaurantId}")
 	public Restaurant findByRestauranttId(@PathVariable Integer restaurantId) {
 		Restaurant restaurant = restaurantRepository.findRestaurauntById(restaurantId);
 		return restaurant;
 	}
+	
+	@PostMapping(value="/restaurant")
+	public void add(@RequestBody Restaurant restaurant) {
+		restaurantRepository.save(restaurant);
+	}
+
+	@PutMapping(value = "/restaurant")
+	public void update(@RequestBody Restaurant restaurant) {
+		restaurantRepository.save(restaurant);
+	}
+	
 	
 }
