b2f959f37474f7c4c654ddbe8c24b8c43b4a2e7d
Alberto <albertoisai15@gmail.com>
2021-05-07 09:02:11 -0500

    Added delete method on restaurant controller

--- src/main/java/com/cognixia/jump/controller/RestaurantController.java
+++ src/main/java/com/cognixia/jump/controller/RestaurantController.java
@@ -1,10 +1,13 @@
 package com.cognixia.jump.controller;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +61,25 @@ public class RestaurantController {
 		
 		return ResponseEntity.created(uri).build();
 		
+	}
+	
+	@DeleteMapping("/restaurants/{id}/delete")
+	public ResponseEntity<String> deleteRestaurant(@PathVariable Integer id) {
+		
+		Optional<Restaurant> found = Optional.ofNullable(repo.findById(id).get());
+		
+		if(found.isPresent()) {
+			
+			repo.deleteById(id);
+			
+			return ResponseEntity.status(200).body("Deleted restaurant with id = " + id);	
+		}
+		else {
+			return ResponseEntity.status(400)
+					.header("restaurant id", id + "")
+					.body("Restaurant with id = " + id + " not found");
+		}
+			
 	}
 	
 	
