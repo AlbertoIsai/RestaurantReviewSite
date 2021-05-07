075229deccf7013edf199a2cdee664ab55dfe70c
Alberto <albertoisai15@gmail.com>
2021-05-07 03:37:31 -0500

    Linked React App
    Linked react app

--- src/main/java/com/cognixia/jump/controller/RestaurantController.java
+++ src/main/java/com/cognixia/jump/controller/RestaurantController.java
@@ -1,45 +1,63 @@
 package com.cognixia.jump.controller;
-
+import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.cognixia.jump.model.Restaurant;
 import com.cognixia.jump.model.Review;
 import com.cognixia.jump.repository.RestaurantRepository;
 
 @RestController
+@CrossOrigin
 public class RestaurantController {
 	
+	
 	@Autowired
-	RestaurantRepository restaurantRepository;
+	private RestaurantRepository repo;
 	
-	@GetMapping(value = "/restaurant")
-	public Iterable<Restaurant> all() {
-		List<Restaurant> restaurants = restaurantRepository.findAll();
-		return restaurants;
+	@GetMapping("/restaurants")
+	public List<Restaurant> getAllRestaurants() {
+		return repo.findAll();
 	}
 	
-	@GetMapping(value = "/restaurant/{restaurantId}")
-	public Restaurant findByRestauranttId(@PathVariable Integer restaurantId) {
-		Restaurant restaurant = restaurantRepository.findRestaurauntById(restaurantId);
-		return restaurant;
+	@GetMapping("/restaurants/{id}")
+	public Restaurant getRestaurantById(@PathVariable Integer id) {
+		return repo.findById(id).get();
 	}
 	
-	@PostMapping(value="/restaurant")
-	public void add(@RequestBody Restaurant restaurant) {
-		restaurantRepository.save(restaurant);
+	
+	// PUT == UPDATE
+	@PutMapping("/restaurants/{id}/edit")
+	public ResponseEntity<Restaurant> updateRestaurant( @PathVariable Integer id, @RequestBody Restaurant restaurant) {
+		
+		Restaurant restaurantUpdated = repo.save(restaurant);
+		
+		return new ResponseEntity<Restaurant>(restaurantUpdated, HttpStatus.OK);
 	}
-
-	@PutMapping(value = "/restaurant")
-	public void update(@RequestBody Restaurant restaurant) {
-		restaurantRepository.save(restaurant);
+	
+	// Post == ADD/Insert
+	@PostMapping("/restaurants/{id}/edit")
+	public ResponseEntity<Restaurant> updateRestaurant( @RequestBody Restaurant restaurant) {
+		
+		Restaurant restaurantCreatedNew = repo.save(restaurant);
+		
+		// Because this is a NEW restaurant, it DOES NOT YET have an ID to path
+		// After we created, we need to append the New ID to the URL path
+		// for example, the new restaurant will be linked to /restaurants/newID
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(restaurantCreatedNew.getRestaurantId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
 	
