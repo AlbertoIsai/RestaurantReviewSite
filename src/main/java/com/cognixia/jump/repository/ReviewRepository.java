0920da204aa5c4420db0718c70858038bec20758
Alberto <albertoisai15@gmail.com>
2021-05-10 07:14:40 -0500

    Fixed Review Repository Query
    Now /restaurants will include reviews array in response

--- src/main/java/com/cognixia/jump/repository/ReviewRepository.java
+++ src/main/java/com/cognixia/jump/repository/ReviewRepository.java
@@ -15,11 +15,11 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	List<Review> findAll();
 
 	//TODO FIX QUERRIES
-	@Query(value = "SELECT * FROM review WHERE user_id = (select user_id from user where user_name = :userName", nativeQuery = true)
+	@Query(value = "SELECT * FROM review WHERE user_id = (select user_id from user where user_name = :userName)", nativeQuery = true)
 	List<Review> findReviewsByUserName(String userName);
 	
 	//TODO FIX QUERRIES
-	@Query(value = "SELECT * FROM review WHERE restaurant_id = (select restaurant_id from restaurant where restaurant_name = :restaurantName", nativeQuery = true)
+	@Query(value = "SELECT * FROM review WHERE restaurant_id = (select restaurant_id from restaurant where restaurant_name = :restaurantName)", nativeQuery = true)
 	List<Review> findReviewsByRestaurantName(String restaurantName);
 	
 	
